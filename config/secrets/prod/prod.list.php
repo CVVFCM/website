4097fbcd223612bf5a5c55a89ceb7608e144c3db
Yohan Giarelli <yohan@les-tilleuls.coop>
2025-11-11 15:26:45 +0000

    feat: Added secret based tokens

---
+++ config/secrets/prod/prod.list.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'CLOUDFLARE_API_TOKEN' => null,
+    'CLOUDFLARE_ZONE_ID' => null,
+    'FACEBOOK_APP_ID' => null,
+    'FACEBOOK_APP_SECRET' => null,
+    'FACEBOOK_PAGE_ID' => null,
+];
